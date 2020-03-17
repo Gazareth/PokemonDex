aee2dcbb74d1f28f41f451a31cc2cd97baccdc08
prsnk <persnicketygareth@gmail.com>
2020-03-17 21:57:51 +0000

    App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+import "typeface-roboto";
+
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { Provider as StoreProvider } from "react-redux";
+import rootReducer from "./store/reducers";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { theme } from "./theme";
+import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import PokedexSVG from "./icons/PokedexIcon";
+
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import SvgIcon from "@material-ui/core/SvgIcon";
+import Typography from "@material-ui/core/Typography";
+
+import MainPage from "./components/pages/MainPage";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles(theme => ({
+  rootBox: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column"
+  },
+  appBar: {
+    position: "relative"
+  }
+}));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box className={classes.rootBox} bgcolor="background.default">
+            <AppBar
+              position="absolute"
+              color="default"
+              className={classes.appBar}
+            >
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  className={classes.menuButton}
+                  color="inherit"
+                  aria-label="menu"
+                >
+                  <SvgIcon
+                    component={PokedexSVG}
+                    fontSize="large"
+                    style={{ fill: "#C30D22" }}
+                    viewBox="0 0 500 525"
+                  />
+                </IconButton>
+
+                <Typography variant="h6" color="textSecondary" noWrap>
+                  Pokémon Dex
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Route
+              exact
+              path={["/", "/home*", "/main*"]}
+              render={() => <MainPage />}
+            />
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </StoreProvider>
   );
-}
+};
 
 export default App;
