2751afb04cb748eb386930817d44364bf0ee44b8
prsnk <persnicketygareth@gmail.com>
2021-01-28 23:03:01 +0000

    fix(copy): Remove hard-coded '947 available' text

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -17,7 +17,7 @@ export const POKEMON_DATA_FIELDS = [
 export const POKEMON_FAVOURITE_FIELDS = ["image", "name", "id", "types"];
 
 export const LOADING_STRINGS = {
-  [SEARCH_POKEMON.NONE]: "947 Available", //@todo make dynamic
+  [SEARCH_POKEMON.NONE]: "Loading...", //@todo make dynamic
   [SEARCH_POKEMON.INIT]: "Starting search...",
   [SEARCH_POKEMON.FOUND]: "Retrieving species...",
   [SEARCH_POKEMON.VARIETY_FOUND]: "Querying evolutions...",
