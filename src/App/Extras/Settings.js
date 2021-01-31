00ec10711610d38e0ac10de3c2bbe81fb6df5be6
prsnk <persnicketygareth@gmail.com>
2021-01-31 20:05:32 +0000

    feat(settings): Add export string functionality

--- src/App/Extras/Settings.js
+++ src/App/Extras/Settings.js
@@ -1,8 +1,8 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { connect } from "react-redux";
 
-import { setThemeMode, setApiInterval } from "Store/actions";
+import { Base64 } from "js-base64";
 
 import SwipeableViews from "react-swipeable-views";
 
@@ -12,6 +12,8 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import TextField from "@material-ui/core/TextField";
+
 import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
@@ -21,19 +23,37 @@ import Slider from "@material-ui/core/Slider";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
 
+import IconButton from "@material-ui/core/IconButton";
+import FileCopyOutlinedIcon from "@material-ui/icons/FileCopyOutlined";
+import ArrowRightAltIcon from "@material-ui/icons/ArrowRightAlt";
+
 import Brightness5Icon from "@material-ui/icons/Brightness5";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 
 import ReportProblemTwoToneIcon from "@material-ui/icons/ReportProblemTwoTone";
-
 import RestoreSharpIcon from "@material-ui/icons/RestoreSharp";
+import AssignmentReturnedIcon from "@material-ui/icons/AssignmentReturned";
 
 import get from "lodash/get";
 import pick from "lodash/pick";
 
+import { setThemeMode, setApiInterval } from "Store/actions";
+
 const useStyles = makeStyles((theme) => {
   const lightMode = theme.palette.type === "light";
   return {
+    impExpWrap: {
+      margin: "2vh 2vw",
+      [theme.breakpoints.down("lg")]: {
+        margin: "2vh 6vw",
+      },
+      [theme.breakpoints.down("md")]: {
+        margin: "2vh 12vw",
+      },
+    },
+    impExpGrid: {
+      height: "100%",
+    },
     form: {
       display: "flex",
       flexDirection: "column",
@@ -68,7 +88,10 @@ const useStyles = makeStyles((theme) => {
     divider2: {
       margin: "32px 0",
     },
-    button: {
+    buttonImpExp: {
+      marginBottom: theme.spacing(2),
+    },
+    buttonClearCache: {
       // margin: "0 0 24px",
       backgroundColor: theme.palette.error.main,
       "&:hover": {
@@ -156,18 +179,24 @@ const SettingsDialog = ({
   apiInterval: stateApiInterval,
   setThemeMode: stateSetThemeMode,
   setApiInterval: stateSetApiInterval,
+  pokemonData,
   clearData,
 }) => {
   const classes = useStyles();
-  console.log("SETTINGS: ", stateApiInterval);
+  const theme = useTheme();
   const initialInterval =
     stateApiInterval || parseInt(process.env.REACT_APP_APIINTERVAL);
 
   const [themeMode, setThemeMode] = useState(stateThemeMode);
   const [apiInterval, setApiInterval] = useState(initialInterval);
   const [clearingCache, setClearingCache] = useState(false);
+  const [impExpMode, setImpExpMode] = useState(true);
 
-  const lightMode = themeMode === "light";
+  const lightMode = useMemo(() => themeMode === "light", [themeMode]);
+  const encodedPokemon = useMemo(
+    () => Base64.encode(JSON.stringify(pokemonData)),
+    [pokemonData]
+  );
 
   const handleDialogClose = (...args) => {
     setClearingCache(false);
@@ -209,9 +238,84 @@ const SettingsDialog = ({
           style={{ ...swipeableStyle }}
           containerStyle={{ height: "100%" }}
           slideStyle={{ ...swipeableStyle }}
-          index={clearingCache ? 1 : 0}
+          index={impExpMode ? 0 : clearingCache ? 2 : 1}
           disabled
         >
+          <div className={classes.impExpWrap}>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="stretch"
+              spacing={5}
+              className={classes.impExpGrid}
+              style={{ overflow: "hidden" }}
+            >
+              <Grid item>
+                <Typography
+                  variant="h6"
+                  color="secondary"
+                  gutterBottom
+                  align="left"
+                >
+                  Export{" "}
+                  <IconButton>
+                    <FileCopyOutlinedIcon />
+                  </IconButton>
+                </Typography>
+                <TextField
+                  multiline
+                  fullWidth
+                  variant="outlined"
+                  rowsMax={6}
+                  rows={6}
+                  aria-label="Export string field"
+                  defaultValue={encodedPokemon}
+                  inputProps={{
+                    readOnly: true,
+                    onClick: (e) => e.target.select(),
+                    style: {
+                      color: theme.palette.text.disabled,
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item>
+                <Typography variant="h6" color="primary" gutterBottom>
+                  Import
+                </Typography>
+                <TextField
+                  multiline={true}
+                  fullWidth
+                  variant="outlined"
+                  rowsMax={6}
+                  rows={6}
+                  aria-label="import string input"
+                  placeholder="Enter import string..."
+                />
+                <div>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ margin: "1rem" }}
+                    onClick={handleClearData}
+                  >
+                    Import data
+                  </Button>
+                </div>
+              </Grid>
+              <Grid item style={{ textAlign: "right" }}>
+                <Button
+                  variant="contained"
+                  color="default"
+                  onClick={() => setImpExpMode(false)}
+                  endIcon={<ArrowRightAltIcon />}
+                >
+                  Back to settings
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
           <div>
             <DialogContentText>
               Tweak the search timing, change between dark or light colour
@@ -263,7 +367,18 @@ const SettingsDialog = ({
                   variant="contained"
                   color="primary"
                   size="large"
-                  className={classes.button}
+                  className={classes.buttonImpExp}
+                  startIcon={<AssignmentReturnedIcon />}
+                  id="clear-data"
+                  onClick={() => setImpExpMode(true)}
+                >
+                  Import/Export data
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  className={classes.buttonClearCache}
                   startIcon={<RestoreSharpIcon />}
                   id="clear-data"
                   onClick={() => setClearingCache(true)}
@@ -295,7 +410,7 @@ const SettingsDialog = ({
                 <i>
                   <u>all</u>
                 </i>{" "}
-                saved data.
+                cached data.
               </Typography>
             </Grid>
             <Grid item>
@@ -333,6 +448,7 @@ const SettingsDialog = ({
 const mapStateToProps = (state) => ({
   themeMode: state.theme.mode,
   apiInterval: state.theme.interval,
+  pokemonData: pick(state, ["pokemon", "favourites"]),
 });
 
 const mapDispatchToProps = {
