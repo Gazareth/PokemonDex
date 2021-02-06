b52371c4517471855b4dcde7047a585bbb4f0267
prsnk <persnicketygareth@gmail.com>
2021-02-06 17:02:38 +0000

    Updates

---
+++ precache-manifest.01b54388ba7d86686930b68d7ef46d2d.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed38a412fa281758e1eba89c94636cb",
+    "url": "/PokemonDex/index.html"
+  },
+  {
+    "revision": "c7c2b89b3e4c00f93d1d",
+    "url": "/PokemonDex/static/css/2.f0f7dc4d.chunk.css"
+  },
+  {
+    "revision": "078c58b4b25b4ab77b94",
+    "url": "/PokemonDex/static/css/main.661e80df.chunk.css"
+  },
+  {
+    "revision": "c7c2b89b3e4c00f93d1d",
+    "url": "/PokemonDex/static/js/2.d3838599.chunk.js"
+  },
+  {
+    "revision": "1583bed55b39251c22a7caf96ba1aeac",
+    "url": "/PokemonDex/static/js/2.d3838599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66aa6d0e46196f15bded",
+    "url": "/PokemonDex/static/js/3.bfaa4131.chunk.js"
+  },
+  {
+    "revision": "196f31064c641ef86b22f34244b7ad03",
+    "url": "/PokemonDex/static/js/3.bfaa4131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "078c58b4b25b4ab77b94",
+    "url": "/PokemonDex/static/js/main.a6564a14.chunk.js"
+  },
+  {
+    "revision": "fc8ca0102a90f63eb4fa",
+    "url": "/PokemonDex/static/js/runtime-main.dd542a16.js"
+  },
+  {
+    "revision": "16319b89a68f9c346de7a58108dc6aeb",
+    "url": "/PokemonDex/static/media/PokeApi.16319b89.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/PokemonDex/static/media/React-icon.37fe8322.png"
+  },
+  {
+    "revision": "97b00882407afb7e6c4e81e46ca5879b",
+    "url": "/PokemonDex/static/media/Visual_Studio_Code_1.35_icon.97b00882.png"
+  },
+  {
+    "revision": "d4611598755c34887548746de137018f",
+    "url": "/PokemonDex/static/media/drawerClose.d4611598.mp3"
+  },
+  {
+    "revision": "078102aa3696782e0df92234c79bf754",
+    "url": "/PokemonDex/static/media/drawerOpen.078102aa.mp3"
+  },
+  {
+    "revision": "08a0704e64eca6451fb3aa0f40a6a298",
+    "url": "/PokemonDex/static/media/favAdded.08a0704e.mp3"
+  },
+  {
+    "revision": "f498cad057c9216f57e6f128ad2ff545",
+    "url": "/PokemonDex/static/media/favMode.f498cad0.mp3"
+  },
+  {
+    "revision": "32049b937bcd0309b7a5a5143b055962",
+    "url": "/PokemonDex/static/media/favRemoved.32049b93.mp3"
+  },
+  {
+    "revision": "adcdcbeb83e2af78e28bd5aa04a34777",
+    "url": "/PokemonDex/static/media/flatIconIcon.adcdcbeb.svg"
+  },
+  {
+    "revision": "ca9e6ac1360b312a427e5a668725a2b8",
+    "url": "/PokemonDex/static/media/lightsOff.ca9e6ac1.mp3"
+  },
+  {
+    "revision": "a5a7b90352e99514a3a03c65df1e1116",
+    "url": "/PokemonDex/static/media/lightsOn.a5a7b903.mp3"
+  },
+  {
+    "revision": "1d5c0e7b1f8b4d33debff8ee26366e26",
+    "url": "/PokemonDex/static/media/pikachu_sound.1d5c0e7b.mp3"
+  },
+  {
+    "revision": "5554cec721809fa8ac35e2573aaf5a0a",
+    "url": "/PokemonDex/static/media/pose-js-icon.5554cec7.png"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/PokemonDex/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/PokemonDex/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/PokemonDex/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/PokemonDex/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/PokemonDex/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/PokemonDex/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/PokemonDex/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/PokemonDex/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/PokemonDex/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/PokemonDex/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/PokemonDex/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/PokemonDex/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/PokemonDex/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/PokemonDex/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/PokemonDex/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/PokemonDex/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/PokemonDex/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/PokemonDex/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/PokemonDex/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/PokemonDex/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/PokemonDex/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/PokemonDex/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/PokemonDex/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/PokemonDex/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "853e026803d3bb4d5697e311f0ea22f5",
+    "url": "/PokemonDex/static/media/searchInit.853e0268.mp3"
+  },
+  {
+    "revision": "6093f1c4b0f5715368f285735d17f54b",
+    "url": "/PokemonDex/static/media/searchMode.6093f1c4.mp3"
+  },
+  {
+    "revision": "e32016eade6f26bedd4f7126edceed3a",
+    "url": "/PokemonDex/static/media/searchPreInit.e32016ea.mp3"
+  },
+  {
+    "revision": "f8f61d22d25742895bf645fd9009e3e5",
+    "url": "/PokemonDex/static/media/untitled.f8f61d22.mp3"
+  },
+  {
+    "revision": "4ebcefeb42fabc6dc2fe4d6927ed4071",
+    "url": "/PokemonDex/static/media/viewMode.4ebcefeb.mp3"
+  }
+]);
